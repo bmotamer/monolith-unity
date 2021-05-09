277db51ff7e41c043fb5501b07f89443aeb04e9d
Bruno Tamer <bmotamer@gmail.com>
2021-05-09 23:56:55 +0300

    Added MathHelper and UnityMathHelper

---
+++ Monolith.Unity/UnityMathHelper.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace Monolith.Unity
+{
+    
+    public static class UnityMathHelper
+    {
+
+        public static float VerticalToHorizontalFieldOfView(Camera camera)
+        {
+            double verticalFieldOfView = camera.fieldOfView * MathHelper.Deg2Rad;
+            double horizontalFieldOfView = MathHelper.VerticalToHorizontalFieldOfView(verticalFieldOfView, camera.aspect);
+            
+            return (float)(horizontalFieldOfView * MathHelper.Rad2Deg);
+        }
+
+        public static Vector2 PixelsToDegrees(Camera camera, float width = 1.0F, float height = 1.0F)
+        {
+            float verticalFieldOfView = camera.fieldOfView;
+            float horizontalFieldOfView = VerticalToHorizontalFieldOfView(camera);
+            
+            return new Vector2(
+                width / camera.pixelWidth * horizontalFieldOfView,
+                height / camera.pixelHeight * verticalFieldOfView
+            );
+        }
+        
+    }
+    
+}
